ddffabe9d9f9a170cd9a19977f0a1d90e4a2edb9
Nadav Borenstein <nborenstein@lightricks.com>
2021-06-05 10:57:59 +0300

    Added funniness_model.py

---
+++ classifiers/funniness_model.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+
+PATH_TO_FUN_CSV = "resources/word-funnines-model/Why_are_some_words_funny_lemmas.csv"
+
+
+class FunModel:
+    """
+    Funniness model. Given a word, the model returns a value describing
+    how funny the word is (lower is better)
+    """
+
+    def __init__(self, type="Arousal"):
+        """
+        builds the funniness model
+        :param type: the funniness measurement type. Default is "Arousal".
+        """
+        self._model = dict()
+        self.type = type
+        data = pd.read_csv(PATH_TO_FUN_CSV)
+        for i, row in data.iterrows():
+            self._model[row[0]] = float(row[type])
+        self._default_value = np.mean(list(self._model.values()))
+
+    def __getitem__(self, word):
+        """
+        Returns the AoA score of "word". If the word is not in the vocabulary an exception is raised
+        """
+        if word in self:
+            return self._model[word]
+        else:
+            return self._default_value
+
+    def __contains__(self, word):
+        """
+        Returns true if "word" is in the vocabulary, and false otherwise
+        """
+        return word in self._model
